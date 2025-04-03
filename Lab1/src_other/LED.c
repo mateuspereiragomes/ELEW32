1b02be26184a18aa91d55a13eede126f0cb4742b
DESKTOP-FTPEKN2\mateu <mateuspereiragomes@gmail.com>
2025-04-03 09:49:44 -0300

    ajuste de comentários do LED

--- Lab1/src_other/LED.c
+++ Lab1/src_other/LED.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------
- * Name:    BTN.c
- * Purpose: Funções de baixo nível para os botões de pressão
- * Note(s): Implementa a interface básica para leitura dos botões PJ0 e PJ1
+ * Name:    LED.c
+ * Purpose: Funções de baixo nível para controle de LEDs
+ * Note(s): Implementa o controle básico para LEDs conectados em PN1, PN0, PF4 e PF0
  *----------------------------------------------------------------------------
  * This file is part of the uVision/ARM development tools.
  * This software may only be used under the terms of a valid, current,
@@ -13,37 +13,77 @@
  * Copyright (c) 2014 Keil - An ARM Company. All rights reserved.
  *----------------------------------------------------------------------------*/
 
-#include <TM4C129.h>  // Inclui as definições específicas do microcontrolador TM4C129
+#include <TM4C129.h>  // Registradores do microcontrolador TM4C129
+#include "LED.h"      // Definições da interface LED
+
+// Mapeamento dos LEDs para seus respectivos pinos:
+// LED0: PN1, LED1: PN0, LED2: PF4, LED3: PF0
+const uint32_t led_mask[] = { 1UL << 1, 1UL << 0, 1UL << 4, 1UL << 0  };
 
 /*----------------------------------------------------------------------------
-  BTN_Initialize - Configura os pinos dos botões (PJ0 e PJ1)
-  Configura os pinos PJ0 e PJ1 como entradas digitais com pull-up
+  LED_Initialize - Configura os pinos dos LEDs como saídas digitais
+  Habilita os clocks e configura os pinos GPIO para os LEDs:
+  - PN1 (LED0), PN0 (LED1) - Port N
+  - PF4 (LED2), PF0 (LED3) - Port F (usando AHB para melhor performance)
  *----------------------------------------------------------------------------*/
-void BTN_Initialize (void) {
-  // Habilita o clock para o GPIO Port J (bit 8 no registrador RCGCGPIO)
-  SYSCTL->RCGCGPIO |= (1ul << 8);                /* enable clock for GPIOs    */
+void LED_Initialize (void) {
+  /* Habilita clock para os GPIO Port N (bit 12) e Port F (bit 5) */
+  SYSCTL->RCGCGPIO |= (1UL << 12) | (1UL << 5);  /* enable clock for GPIOs    */
   
-  // Configura os pinos PJ0 e PJ1 para drive de 2mA (DR2R = 2mA Drive Select)
-  GPIOJ_AHB->DR2R |=  ((1ul << 0) | (1ul << 1)); /* PJ0, PJ1 2-mA Drive       */
+  /* Configura Port N - LEDs 0 e 1 */
+  GPION->DIR     |= led_mask[0] | led_mask[1];   /* PN1, PN0 como saídas      */
+  GPION->DEN     |= led_mask[0] | led_mask[1];   /* Habilita função digital   */
   
-  // Habilita resistores de pull-up internos para PJ0 e PJ1
-  GPIOJ_AHB->PUR  |=  ((1ul << 0) | (1ul << 1)); /* PJ0, PJ1 pull-up          */
-  
-  // Configura PJ0 e PJ1 como entradas (DIR = Direction, 0 = input)
-  GPIOJ_AHB->DIR  &= ~((1ul << 0) | (1ul << 1)); /* PJ0, PJ1 is intput        */
-  
-  // Habilita a função digital para PJ0 e PJ1 (DEN = Digital Enable)
-  GPIOJ_AHB->DEN  |=  ((1ul << 0) | (1ul << 1)); /* PJ0, PJ1 is digital func. */
+  /* Configura Port F (AHB) - LEDs 2 e 3 */
+  GPIOF_AHB->DIR |= led_mask[2] | led_mask[3];   /* PF4, PF0 como saídas      */
+  GPIOF_AHB->DEN |= led_mask[2] | led_mask[3];   /* Habilita função digital   */
 }
 
 /*----------------------------------------------------------------------------
-  BTN_Get - Lê o estado dos botões
-  @return: Máscara de bits onde cada bit representa o estado de um botão (1 = pressionado)
-  Nota: A lógica é invertida (~) porque os botões estão configurados com pull-up
-        (0V quando pressionado, 3.3V quando solto)
+  LED_On - Acende um LED específico
+  @param num: Número do LED (0 a LED_NUM-1)
+  Operação atômica via escrita no registrador DATA (seta o bit correspondente)
  *----------------------------------------------------------------------------*/
-uint32_t BTN_Get (void) {
-  // Lê o registrador DATA e aplica máscara para PJ0 e PJ1
-  // Inverte os bits (~) porque a lógica é ativa-baixa (pull-up)
-  return (~(GPIOJ_AHB->DATA) & ((1ul << 0) | (1ul << 1)));
+void LED_On (uint32_t num) {
+  if (num < LED_NUM) {  // Verifica se o LED é válido
+    switch (num) {
+      case 0: GPION->DATA     |=  led_mask[num]; break;  // LED0 (PN1)
+      case 1: GPION->DATA     |=  led_mask[num]; break;  // LED1 (PN0)
+      case 2: GPIOF_AHB->DATA |=  led_mask[num]; break;  // LED2 (PF4)
+      case 3: GPIOF_AHB->DATA |=  led_mask[num]; break;  // LED3 (PF0)
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------
+  LED_Off - Apaga um LED específico
+  @param num: Número do LED (0 a LED_NUM-1)
+  Operação atômica via escrita no registrador DATA (limpa o bit correspondente)
+ *----------------------------------------------------------------------------*/
+void LED_Off (uint32_t num) {
+  if (num < LED_NUM) {  // Verifica se o LED é válido
+    switch (num) {
+      case 0: GPION->DATA     &= ~led_mask[num]; break;  // LED0 (PN1)
+      case 1: GPION->DATA     &= ~led_mask[num]; break;  // LED1 (PN0)
+      case 2: GPIOF_AHB->DATA &= ~led_mask[num]; break;  // LED2 (PF4)
+      case 3: GPIOF_AHB->DATA &= ~led_mask[num]; break;  // LED3 (PF0)
+    }
+  }
+}
+
+/*----------------------------------------------------------------------------
+  LED_Out - Controla todos os LEDs simultaneamente com um padrão de bits
+  @param value: Máscara de bits onde cada bit representa o estado de um LED
+  (bit 0 = LED0, bit 1 = LED1, etc. - 1 = aceso, 0 = apagado)
+ *----------------------------------------------------------------------------*/
+void LED_Out(uint32_t value) {
+  int i;
+
+  for (i = 0; i < LED_NUM; i++) {  // Itera por cada LED
+    if (value & (1<<i)) {          // Verifica se o bit correspondente está setado
+      LED_On (i);                  // Acende o LED se o bit for 1
+    } else {
+      LED_Off(i);                  // Apaga o LED se o bit for 0
+    }
+  }
 }
